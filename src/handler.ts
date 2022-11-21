323f5fbd3407288f6dd9e29e646e360200d990d5
krishna-swivel <krishnamohan@swivelgroup.com.au>
2022-11-21 14:04:14 +0530

    Added

---
+++ src/handler.ts
@@ -0,0 +1,98 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import AWS from 'aws-sdk';
+import * as yup from 'yup';
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const tableName = "TitleTable";
+
+
+const headers = {
+  "content-type": "application/json",
+};
+
+const schema = yup.object().shape({
+  id : yup.string(),
+  title: yup.string(),
+  url : yup.string(),
+});
+
+
+class HttpError extends Error {
+  constructor(public statusCode: number, body: Record<string, unknown> = {}) {
+    super(JSON.stringify(body));
+  }
+}
+
+const handleError = (e: unknown) => {
+  if (e instanceof yup.ValidationError) {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({
+        errors: e.errors,
+      }),
+    };
+  }
+
+  if (e instanceof SyntaxError) {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({ error: `invalid request body format : "${e.message}"` }),
+    };
+  }
+
+  if (e instanceof HttpError) {
+    return {
+      statusCode: e.statusCode,
+      headers,
+      body: e.message,
+    };
+  }
+
+  throw e;
+};
+
+
+export const createLink = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const reqBody = JSON.parse(event.body as string);
+
+    await schema.validate(reqBody, { abortEarly: false });
+
+    const product = {
+      ...reqBody,
+    };
+
+    await docClient
+      .put({
+        TableName: tableName,
+        Item: product,
+      })
+      .promise();
+
+    return {
+      statusCode: 201,
+      headers,
+      body: JSON.stringify(product),
+    };
+  } catch (e) {
+    return handleError(e);
+  }
+};
+
+export const listLink = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const output = await docClient
+    .scan({
+      TableName: tableName,
+    })
+    .promise();
+
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify(output.Items),
+  };
+};
+
