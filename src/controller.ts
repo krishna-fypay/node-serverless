92a4fd0ca14e61445502dbf1c11128787da9d9fc
krishna-swivel <krishnamohan@swivelgroup.com.au>
2022-11-22 10:27:12 +0530

    changes added

---
+++ src/controller.ts
@@ -0,0 +1,157 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import AWS from 'aws-sdk';
+import * as yup from 'yup';
+import { v4 as uuidv4 } from 'uuid';
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const tableName = "QuizTable";
+
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  "content-type": "application/json",
+};
+
+const schema = yup.object().shape({
+  id: yup.string(),
+  questions: yup.string(),
+  type : yup.string(),
+  answers: yup
+    .array()
+    .of(
+      yup.object().shape({
+        options: yup.string(),
+        correct : yup.boolean()
+      }),
+    )
+    .required("Required"),
+});
+
+
+class HttpError extends Error {
+  constructor(public statusCode: number, body: Record<string, unknown> = {}) {
+    super(JSON.stringify(body));
+  }
+}
+
+const handleError = (e: unknown) => {
+  if (e instanceof yup.ValidationError) {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({
+        errors: e.errors,
+      }),
+    };
+  }
+
+  if (e instanceof SyntaxError) {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({ error: `invalid request body format : "${e.message}"` }),
+    };
+  }
+
+  if (e instanceof HttpError) {
+    return {
+      statusCode: e.statusCode,
+      headers,
+      body: e.message,
+    };
+  }
+
+  throw e;
+};
+
+export const createQuiz = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const reqBody = JSON.parse(event.body as string);
+
+    await schema.validate(reqBody, { abortEarly: false });
+
+      const product = {
+         id: uuidv4(),
+      ...reqBody,
+    };
+
+    await docClient
+      .put({
+        TableName: tableName,
+        Item: product,
+      })
+      .promise();
+
+    return {
+      statusCode: 201,
+      headers,
+      body: JSON.stringify(product),
+    };
+  } catch (e) {
+    return handleError(e);
+  }
+};
+
+export const listQuiz = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const output = await docClient
+    .scan({
+      TableName: tableName,
+    })
+    .promise();
+
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify(output.Items),
+  };
+};
+
+export const getQuestionById = async (id: string) => {
+  const output = await docClient
+    .get({
+      TableName: tableName,
+      Key: {
+        id: id,
+      },
+    })
+    .promise();
+
+  if (!output.Item) {
+    throw new HttpError(404, { error: "not found" });
+  }
+};
+
+export const getQuestion = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const product = await getQuestionById(event.pathParameters?.id as string);
+
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(product),
+    };
+  } catch (e) {
+    return handleError(e);
+  }
+};
+export const deleteQuestion = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  try {
+    const id = event.pathParameters?.id as string;
+
+    await docClient
+      .delete({
+        TableName: tableName,
+        Key: {
+          id: id,
+        },
+      })
+      .promise();
+
+    return {
+      statusCode: 204,
+      body: "",
+    };
+  } catch (e) {
+    return handleError(e);
+  }
+};
